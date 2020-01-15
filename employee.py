41c592dcdfd97f9cb10a1c6d32dfbe8e3e11f7a7
Adrian Corsini <glenflesk@gmail.com>
2020-01-15 18:13:34 +0000

    learning about class variables

--- employee.py
+++ employee.py
@@ -1,4 +1,6 @@
 class Employee:
+
+    raise_amount = 1.04
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -9,13 +11,15 @@ class Employee:
         return '{} {}'.format(self.first, self.last)
 
 
+    def apply_raise(self):
+        self.pay = int(self.pay * self.aise_amount)
+    
 emp_1 = Employee("Adrian","Corsini", 50000)
 emp_2 = Employee("Steven","Corsini", 60000)
 
+print(emp_1.__dict__)
+print(Employee.__dict__)
+print(emp_1.raise_amount)
+print(Employee.raise_amount)
+print(emp_2.raise_amount)
 
-print(emp_1.email)
-print(emp_2.email)
-print(emp_1.fullname())
-print(emp_2.fullname())
-
-print(Employee.fullname(emp_1))
