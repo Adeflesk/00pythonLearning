d3d276ba7d0f9ddc435a193b52815ba7d79b38fc
Adrian Corsini <glenflesk@gmail.com>
2020-01-15 18:45:18 +0000

    Finished tutorial classmethods and static methods

--- employee.py
+++ employee.py
@@ -15,14 +15,35 @@ class Employee:
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
-    
+
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first , last, pay = emp_str.split('-')
+        return cls(first, last,pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday()== 5 or day.weekday()== 6:
+            return False
+        return True
+
+         
 emp_1 = Employee("Adrian","Corsini", 50000)
 emp_2 = Employee("Steven","Corsini", 60000)
+emp_3 = Employee.from_string('Sarah-Corsini-45000')
 
-print(emp_1.__dict__)
-print(Employee.__dict__)
-print(emp_1.raise_amount)
+
+Employee.set_raise_amount(1.05)
+
 print(Employee.raise_amount)
+print(emp_1.raise_amount)
 print(emp_2.raise_amount)
 print(Employee.num_of_emps)
-
+print(emp_3.fullname())
+import datetime
+my_date = datetime.date(2018, 8, 10)
+print(Employee.is_workday(my_date))
